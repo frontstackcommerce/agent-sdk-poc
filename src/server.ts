84b4d9258f21d0476fed702064d5b463794a57e3
Uwe Kleinmann <uwe@kleinmann.org>
2026-02-03 12:57:28 +0100

    Parse JSON bodies.

--- src/server.ts
+++ src/server.ts
@@ -40,6 +40,9 @@ const connectionManager = new ConnectionManager();
 
 const app = express();
 const port = 8080;
+
+app.use(express.json())
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
