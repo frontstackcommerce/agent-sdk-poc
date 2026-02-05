2f18bcd2c3d9a55d8ef2a827103459926f42d968
Uwe Kleinmann <uwe@kleinmann.org>
2026-02-05 12:23:35 +0100

    NTR Remove query components that are now passed via API.

--- src/agent.ts
+++ src/agent.ts
@@ -1,6 +1,5 @@
 import { query, Options, SDKUserMessage, SDKMessage, HookCallback, McpServerConfig, AgentDefinition } from "@anthropic-ai/claude-agent-sdk";
 import path from "node:path";
-import fs from "node:fs";
 import { ConnectionManager } from "./server";
 
 export type Configuration = {
@@ -23,16 +22,6 @@ export type Configuration = {
   mcpServers?: Record<string, McpServerConfig>
   systemPrompt?: string
 };
-
-const AGENT_SDK_MCP_TOOLS = {
-  READ: "Read",
-  EDIT: "Edit",
-  GLOB: "Glob",
-  TASK: "Task",
-  WRITE: "Write",
-  ASK_USER_QUESTION: "AskUserQuestion",
-  SKILL: "Skill",
-}
 
 let sessionId = "";
 let transcriptPath = "";
@@ -64,35 +53,9 @@ export function getAbortController(): AbortController {
 }
 
 const AGENT_OPTIONS: Options = {
-  // TODO: Remove and pass-through via chat call
-  systemPrompt:
-    fs.readFileSync(path.join(import.meta.dirname, "..", "sub-agents", "buddy.md"), "utf8"),
-  tools: [...Object.values(AGENT_SDK_MCP_TOOLS)],
-  // TODO: Remove and pass-through via chat call
-  allowedTools: [
-    // Built-in tools
-    ...Object.values(AGENT_SDK_MCP_TOOLS),
-  ],
   permissionMode: "acceptEdits", // plan = creates a plan file, acceptEdits = accepts the edits and returns the result
   model: "sonnet",
   includePartialMessages: true, // Enable streaming of partial messages for real-time output
-  // TODO: Move agents to Frontic and pass-through via chat call
-  agents: {
-    "frontend-engineer": {
-      prompt:
-        fs.readFileSync(path.join(import.meta.dirname, "..", "sub-agents", "frontend-engineer.md"), "utf8"),
-      description:
-        "Senior frontend engineer who writes and modifies frontend code. Use when you need to CREATE or MODIFY code files. The engineer works independently and returns results when done.",
-      tools: [...Object.values(AGENT_SDK_MCP_TOOLS), "Bash"]
-    },
-    "api-agent": {
-      prompt:
-        fs.readFileSync(path.join(import.meta.dirname, "..", "sub-agents", "api-agent.md"), "utf8"),
-      description:
-        "API agent who creates and modifies API endpoints. Use when you need to CREATE or MODIFY API endpoints. The agent works independently and returns results when done.",
-      tools: [...Object.values(AGENT_SDK_MCP_TOOLS)],
-    },
-  },
   settingSources: ["user"],
   hooks: {
     UserPromptSubmit: [{
@@ -142,10 +105,6 @@ export const runAgent = async (userPrompt: string, connectionManager: Connection
     options: {
       ...AGENT_OPTIONS,
       ...configuration,
-      allowedTools: [
-        ...(AGENT_OPTIONS.allowedTools ?? []),
-        ...(configuration.allowedTools ?? []),
-      ],
       continue: shouldContinueConversation,
     },
   })) {
